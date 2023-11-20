8e8d7d9c1d32d134f1c220c0e4effcc5ebb5b473
문찬용[물류데이터플랫폼개발] <chanyong.moon@navercorp.com>
2023-11-20 20:00:11 +0900

    deploy page

--- front/src/views/analysis/general/markdown/signup-series-analysis.js
+++
@@ -1,9 +0,0 @@
-
-export default  `
-
-The trend of X account registrations was visualized using a time series graph.
-
-X showed a steep increase from September 2019 to May 2020.
-
-Reviewing Google Trends from August 2019 to April 2022 for topics and search terms related to 'China,' it can be speculated that the increase in direct social media activity might have been due to a worsening external image associated with COVID-19.
-`
